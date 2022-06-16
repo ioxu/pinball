7e9bb49020472f66dc34d0e72d39b428e6a5e019
ioxu <ben@ioxu.com>
2022-06-16 16:33:33 +0100

    added ball-following camera
    sorted out level-in-viewports, etc

--- screens/game.gd
+++ screens/game.gd
@@ -5,18 +5,20 @@ extends Node2D
 
 onready var first_level = load("res://levels/test_level.tscn")
 
+var window_size = Vector2.ZERO
+
 func _ready():
 	var _ws = OS.get_window_size()
+	window_size = _ws
 	print("game window size %s"%_ws)
-	
 	print("setting " + $ViewportContainer/Viewport.get_path()+"'s resolution to %s"%[_ws])
-	$ViewportContainer/Viewport.set_size( _ws )
+	$ViewportContainer/Viewport.set_size( Vector2( _ws.x, _ws.y ))
 	switch_level( first_level )
 
 
 func _process(_delta):
-	#$fps_label.text = str(Performance.get_monitor(Performance.TIME_FPS))
 	$fps_label.text = str(Engine.get_frames_per_second())
+
 
 
 func _input(_event):
@@ -28,3 +30,18 @@ func switch_level( level ) -> void :
 	print("switch level %s"%str(level) )
 	var lvl = level.instance()
 	$ViewportContainer/Viewport.add_child(lvl)
+	
+	#===========================================================================
+	# find balls, tell camera
+	print(".. find balls")
+	var balls = []
+	for c in lvl.get_children():
+		if c.get_class() == "Pinball":
+			balls.append(c)
+	
+	if len(balls) > 0:
+		print("found %s balls (%s)"%[len(balls), balls])
+
+	for b in balls:
+		$ViewportContainer/Viewport/Camera2D.add_tracked_object(b)
+	#===========================================================================
